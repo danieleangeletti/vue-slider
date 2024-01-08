5dae1472e81bdaa0b4acad96ac84654990b14ca9
danieleangeletti <angelettidaniele@gmail.com>
2024-01-08 15:35:36 +0000

    ADD pulsanti per scorrere le img

--- js/script.js
+++ js/script.js
@@ -14,27 +14,44 @@ createApp({
                     image: 'img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-                    visibility: 'invisible'
+                    visibility: ''
                 },
                 {
                     image: 'img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-                    visibility: 'invisible'
+                    visibility: ''
                 },
                 {
                     image: 'img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-                    visibility: 'invisible'
+                    visibility: ''
                 },
                 {
                     image: 'img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-                    visibility: 'invisible'
+                    visibility: ''
                 }
-            ]
+            ],
+
+            current_slide: 0
+
         };
+    },
+
+    methods: {
+        next_image(){
+            this.slides[this.current_slide].visibility = '';
+            this.current_slide++;
+            this.slides[this.current_slide].visibility = 'visible';
+        },
+
+        previous_image(){
+            this.slides[this.current_slide].visibility = '';
+            this.current_slide--;
+            this.slides[this.current_slide].visibility = 'visible';
+        }
     }
 }).mount('#app');
